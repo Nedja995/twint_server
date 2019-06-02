468211bf56a17ad9e6febb57a4344760c1cbe1ab
Nedja995 <nedjapejasinovic@gmail.com>
2019-06-02 17:10:42 +0200

    minor bugfix

--- tasks.py
+++ tasks.py
@@ -6,16 +6,16 @@ from arguments import TwintArguments
 
 @celery.task(name='fetch')
 def fetch(args):
-    print("Start fetch task %s %s -> %s" % 
-        (args.id, config.Since, config.Until))
     # Merge user arguments with default Twint config
     config = TwintArguments()
     config.__dict__.update(args)
+    print("Start fetch task %s %s -> %s" % 
+        (config.id, config.Since, config.Until))
     # Run
     twint.run.Search(config)
     # Finished
-    return "Fetch task finished %s %s -> %s" % 
-        (args.id, config.Since, config.Until)
+    return "Fetch task finished %s %s -> %s" % \
+        (config.id, config.Since, config.Until)
 
 # # Save/report progress if you want or example to firebase
 # @celery.task(name='save')
